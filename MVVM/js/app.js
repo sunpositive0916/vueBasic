26a331bac9c26e997202291a39b57badbb77e9b4
MOON <sunpositive7@gmail.com>
2022-02-16 22:32:46 +0900

    [Vue] 검색폼 구현

--- MVVM/js/app.js
+++ MVVM/js/app.js
@@ -4,6 +4,20 @@ new Vue({
   el: '#app',
   // 전달할 데이터
   data: {
-    msg: 'hello world'
+    // 입력을 데이터를 받아서 저장하는 역할
+    query: '',
+  },
+  // DOM과 바인딩할 함수 정의
+  methods:{
+    onSubmit(e){
+      debugger
+    },
+    onKeyup(){
+      if(!this.query.length) this.onReset()
+    },
+    onReset(){
+      this.query = ''
+      // 검색 결과를 숨기는 코드 작성 예정
+    }
   }
 })
