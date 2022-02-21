6bc9da51e5985a94d3563c24be0b7914ebe2bf0b
MOON <sunpositive7@gmail.com>
2022-02-21 21:23:53 +0900

    컴포넌트화 이전 코드 올리기

---
+++ MVVMcomponent/js/app.js
@@ -0,0 +1,75 @@
+import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
+import HistoryModel from './models/HistoryModel.js'
+
+// Vue Instance 생성
+new Vue({
+  // Vue Instance가 HTML의 어느 부분에 마운팅 될지
+  el: '#app',
+  // 전달할 데이터
+  data: {
+    // 입력을 데이터를 받아서 저장하는 역할
+    query: '',
+    // submit이 일어났는지 안일어났는지 확인(검색어를 칠 때 마다 "'검색어' 검색어로 찾을 수 없습니다." 문구가 뜨기 때문)
+    submitted: false,
+    // 탭은 두개니까 배열로
+    tabs: ['추천 검색어', '최근 검색어'],
+    selectedTab: '',
+    // 검색결과
+    keywords: [],
+    history: [],
+    searchResult: [],
+  },
+  // 뷰 인스턴스가 생성 될 때 실행
+  created(){
+    this.selectedTab = this.tabs[0]
+    this.fetchKeyword()
+    this.fetchHistory()
+  },
+  // DOM과 바인딩할 함수 정의
+  methods:{
+    onSubmit(e){
+      this.search()
+    },
+    onKeyup(){
+      if(!this.query.length) this.resetForm()
+    },
+    onReset(){
+      this.resetForm()
+    },
+    onClickTab(tab){
+      this.selectedTab = tab
+    },
+    onClickKeyword(keyowrd){
+      this.query = keyowrd
+      this.search()
+    },
+    onClickRemoveHistory(keyword){
+      HistoryModel.remove(keyword)
+      this.fetchHistory()
+    },
+    fetchKeyword(){
+      KeywordModel.list().then(data => {
+        this.keywords = data
+      })
+    },
+    fetchHistory(){
+      HistoryModel.list().then(data => {
+        this.history = data
+      })
+    },
+    search(){
+      SearchModel.list().then(data => {
+        this.submitted = true
+        this.searchResult = data
+      })
+      HistoryModel.add(this.query)
+      this.fetchHistory()
+    },
+    resetForm(){
+      this.query = ''
+      this.submitted = false
+      this.searchResult = []
+    }
+  }
+})
